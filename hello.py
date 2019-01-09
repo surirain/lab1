becba522134725c2607878f81b145a32e591bbbb
surirain <freesoul.el@gmail.com>
2019-01-09 15:24:10 -0600

    name change

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
-name = 'world'
+name = 'freesoul'
 
 print('Hello, ' + name + '!')
--- hello_typed.py
+++ hello_typed.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
-name: str = 'world'
+name: int = 'world'
 
 print('Hello, ' + name + '!')
